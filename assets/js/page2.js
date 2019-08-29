53fca3ba6732a5ee43c4d38e528dc6a138af128e
Kieran <brett.kieran@hotmail.com>
2019-08-29 13:04:17 +1200

    Updated Website

--- assets/js/page2.js
+++ assets/js/page2.js
@@ -12,6 +12,11 @@ text.addEventListener('click', () => { // TXT EVENT
             console.log("File Not Found");
         }
     }
+
+    xhr.onerror = () =>{
+        console.log(`Current Load State ${xhr.readyState}`)
+    }
+
     xhr.send();
 });
 
@@ -32,8 +37,42 @@ json.addEventListener('click', () => { // JSON EVENT
     }
 
     xhr.onerror = () =>{
-        console.log(`Current Load State ${xhr.readyState}`)
+        console.log(`Current Load State ${xhr.readyState}`);
     }
 
     xhr.send();
 });
+
+
+
+// PHP Code 
+
+const form = document.getElementById('php-form');
+
+const sendPhp = (e) => {
+    e.preventDefault();
+    const firstName = document.getElementById('first-name').value;
+    const lastName = document.getElementById('last-name').value;
+    const emailAddress = document.getElementById('email-address').value;
+
+    const params = `firstName=${firstName}&lastName=${lastName}&emailAddress=${emailAddress}`;
+    xhr.open('POST', 'http://kate.ict.op.ac.nz/~orrgl1/server.php', true);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded'); // Going to use content type of this query string
+    xhr.onload = () => {
+        if (xhr.status === 200){
+            console.log(xhr.responseText);
+            const formRes = document.getElementById('php')
+            formRes.textContent = `${xhr.responseText}`
+            formRes.style.color = "red"
+            formRes.style.background = "white"
+            formRes.style.border = "thick solid #000"
+            formRes.style.padding = "12px"
+        } 
+        else if (xhr.status === 500){ // 500 is internal server error
+            console.log("Internal server error");
+        }
+    }
+    xhr.send(params);
+}
+
+form.addEventListener('submit', sendPhp);
