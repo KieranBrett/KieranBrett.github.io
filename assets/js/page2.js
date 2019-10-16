c407da415dade28a1e568e8092ac5bb08a684bc4
KieranBrett <brett.kieran@Hotmail.com>
2019-10-16 23:06:49 +1300

    Update

--- assets/js/page2.js
+++ assets/js/page2.js
@@ -1,22 +1,23 @@
-const items = document.getElementById('items');
-const form = document.getElementById('comment-form');
-const comments = document.getElementById('comment-area');
+const items = document.getElementById("items");
+const form = document.getElementById("comment-form");
+const comments = document.getElementById("comment-area");
 let savedComments = {};
 
 const xhr = new XMLHttpRequest();
 
+xhr.open("GET", "motorbikes.json", true);
+xhr.onload = () => {
+  if (xhr.status === 200) {
+    const per = JSON.parse(xhr.responseText);
+    //console.log(per);
 
-    xhr.open('GET', 'motorbikes.json', true);
-    xhr.onload = () => {
-        if (xhr.status === 200){
-            const per = JSON.parse(xhr.responseText);
-            //console.log(per);
+    per.forEach(p => {
+      // let list = document.getElementById('json');
+      // list.innerHTML = list.innerHTML + `<li>${p.lastName}, ${p.firstName} ID: ${p.id}`;
 
-            per.forEach(p => {
-                // let list = document.getElementById('json');
-                // list.innerHTML = list.innerHTML + `<li>${p.lastName}, ${p.firstName} ID: ${p.id}`;
-
-                items.innerHTML = items.innerHTML + `<div class="frames">
+      items.innerHTML =
+        items.innerHTML +
+        `<div class="frames">
                                                     <div class="images"> 
                                                         <img class="image" src="${p.imgsrc}" height="250"> 
                                                     </div> 
@@ -29,50 +30,46 @@ const xhr = new XMLHttpRequest();
                                                             <p class="link"><a href="${p.website}">Click here for the manufacturers website</a></p>
                                                         </div>
 
-                                                    </div></br>`
-            })
-        }
-        else if (xhr.status === 404){
-            console.log("File Not Found");
-        }
-    }
+                                                    </div></br>`;
+    });
+  } else if (xhr.status === 404) {
+    console.log("File Not Found");
+  }
+};
 
-    xhr.onerror = () =>{
-        console.log(`Current Load State ${xhr.readyState}`);
-    }
+xhr.onerror = () => {
+  console.log(`Current Load State ${xhr.readyState}`);
+};
 
-    xhr.send();
+xhr.send();
 
-    fetch("motorbikeComments.json")
-    .then(response => response.json())
-    .then(data => {
-        savedComments = data;
+fetch("motorbikeComments.json")
+  .then(response => response.json())
+  .then(data => {
+    savedComments = data;
 
-        savedComments.forEach(c =>{
-            console.log(c.name);
-            comments.innerHTML = `<div class="comment">
+    savedComments.forEach(c => {
+      console.log(c.name);
+      comments.innerHTML =
+        `<div class="comment">
                                     <p class="name">${c.name}</p> 
                                     <p class="time">${c.time}</p>    <br>
                                     <p class="comment-text">${c.comment}</p>
                                     <br>
                                 </div> <br>
                             ` + comments.innerHTML;
-        })
-    })
+    });
+  });
 
+const addComment = e => {
+  e.preventDefault();
+  const name = document.getElementById("name-input").value;
+  const comment = document.getElementById("comment").value;
+  let time = new Date();
+  let timeString = `${time.getFullYear()}/${time.getMonth()}/${time.getDate()} - ${time.getHours()}:${time.getMinutes()}`;
 
-
-    
-
-    const addComment = (e) => {
-        
-        e.preventDefault();
-        const name = document.getElementById('name-input').value;
-        const comment = document.getElementById('comment').value;
-        let time = new Date();
-        let timeString = `${time.getFullYear()}/${time.getMonth()}/${time.getDate()} - ${time.getHours()}:${time.getMinutes()}`
-        
-        comments.innerHTML = `<div class="comment">
+  comments.innerHTML =
+    `<div class="comment">
                                     <p class="name">${name}</p> 
                                     <p class="time">${timeString}</p>    <br>
                                     <p class="comment-text">${comment}</p>
@@ -80,5 +77,5 @@ const xhr = new XMLHttpRequest();
                                 </div> <br>
                             ` + comments.innerHTML;
 };
-    
-    form.addEventListener('submit', addComment);
+
+form.addEventListener("submit", addComment);
--- assets/js/page3.js
+++ assets/js/page3.js
@@ -1,19 +1,20 @@
-const items = document.getElementById('items');
+const items = document.getElementById("items");
 
 const xhr = new XMLHttpRequest();
 
+xhr.open("GET", "helmets.json", true);
+xhr.onload = () => {
+  if (xhr.status === 200) {
+    const per = JSON.parse(xhr.responseText);
+    //console.log(per);
 
-    xhr.open('GET', 'helmets.json', true);
-    xhr.onload = () => {
-        if (xhr.status === 200){
-            const per = JSON.parse(xhr.responseText);
-            //console.log(per);
+    per.forEach(p => {
+      // let list = document.getElementById('json');
+      // list.innerHTML = list.innerHTML + `<li>${p.lastName}, ${p.firstName} ID: ${p.id}`;
 
-            per.forEach(p => {
-                // let list = document.getElementById('json');
-                // list.innerHTML = list.innerHTML + `<li>${p.lastName}, ${p.firstName} ID: ${p.id}`;
-
-                items.innerHTML = items.innerHTML + `<div class="frames">
+      items.innerHTML =
+        items.innerHTML +
+        `<div class="frames">
                                                     <div class="images"> 
                                                         <img src="${p.imgsrc}" height="250"> </div> 
 
@@ -25,42 +26,37 @@ const xhr = new XMLHttpRequest();
                                                             <p class="link"><a href="${p.website}">Click here for the manufacturers website</a></p>
                                                         </div>
 
-                                                    </div></br>`
-            })
-        }
-        else if (xhr.status === 404){
-            console.log("File Not Found");
-        }
-    }
+                                                    </div></br>`;
+    });
+  } else if (xhr.status === 404) {
+    console.log("File Not Found");
+  }
+};
 
-    xhr.onerror = () =>{
-        console.log(`Current Load State ${xhr.readyState}`);
-    }
+xhr.onerror = () => {
+  console.log(`Current Load State ${xhr.readyState}`);
+};
 
-    xhr.send();
+xhr.send();
 
+const form = document.getElementById("comment-form");
+const comments = document.getElementById("comment-area");
 
+const addComment = e => {
+  e.preventDefault();
+  const name = document.getElementById("name-input").value;
+  const comment = document.getElementById("comment").value;
+  let time = new Date();
+  let timeString = `${time.getFullYear()}/${time.getMonth()}/${time.getDate()} - ${time.getHours()}:${time.getMinutes()}`;
 
-
-    const form = document.getElementById('comment-form');
-    const comments = document.getElementById('comment-area');
-
-    const addComment = (e) => {
-        
-        e.preventDefault();
-        const name = document.getElementById('name-input').value;
-        const comment = document.getElementById('comment').value;
-        let time = new Date();
-        let timeString = `${time.getFullYear()}/${time.getMonth()}/${time.getDate()} - ${time.getHours()}:${time.getMinutes()}`
-
-        comments.innerHTML = `<div class="comment">
+  comments.innerHTML =
+    `<div class="comment">
                                     <p class="name">${name}</p> 
                                     <p class="time">${timeString}</p>    <br>
                                     <p class="comment-text">${comment}</p>
                                     <br>
                                 </div> <br>
                             ` + comments.innerHTML;
-        
-    };
-    
-    form.addEventListener('submit', addComment);
+};
+
+form.addEventListener("submit", addComment);
