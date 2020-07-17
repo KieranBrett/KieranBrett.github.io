e59fa71bf7e9a830f6dbcfcb7f5dd3b46ebaa35a
KieranBrett <brett.kieran@hotmail.com>
2020-07-17 16:52:21 +1200

    updaded colour scheme, added contact form

--- assets/sideBar.js
+++ assets/sideBar.js
@@ -5,12 +5,14 @@ let navButton = document.getElementById('navButton');
 function minimizeBar() {
     sideBar.style.left = '-270px'; // Gives smooth transition effect to move entire div off screen than set hidden
     contentPane.style.marginLeft = '0px';
+    contentPane.style.paddingLeft = '10%';
     sideBar.style.visibility = 'hidden';
 }
 
 function expandBar() { 
     sideBar.style.left = '0px' // Setting div to be on screen
     contentPane.style.marginLeft = '260px'; // Setting margin for content pane
+    contentPane.style.paddingLeft = '0%';
     sideBar.style.visibility = 'visible'; // Showing sidebar
 }
 
