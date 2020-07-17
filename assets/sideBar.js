9fa4779950aea8068db8a8bfd070890cee91a661
KieranBrett <brett.kieran@hotmail.com>
2020-07-17 15:32:08 +1200

    Added navBar and sidebar can be minimized

---
+++ assets/sideBar.js
@@ -0,0 +1,24 @@
+let sideBar = document.getElementById('sideBar');
+let contentPane = document.getElementById('contentPane');
+
+function minimizeBar() {
+    sideBar.style.left = '-270px'; // Gives smooth transition effect to move entire div off screen than set hidden
+    contentPane.style.marginLeft = '0px';
+    sideBar.style.visibility = 'hidden';
+}
+
+function expandBar() { 
+    sideBar.style.left = '0px' // Setting div to be on screen
+    contentPane.style.marginLeft = '260px'; // Setting margin for content pane
+    sideBar.style.visibility = 'visible'; // Showing sidebar
+}
+
+document.getElementById('navButton').onclick = () => {
+    if (sideBar.style.left == '-270px'){ // If the div is off the screen
+        expandBar();                   // Expand it
+    }
+    else{
+        minimizeBar();                  // Minimize It
+    }
+    
+}
