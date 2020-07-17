a62a3204bdb3f059efe2254d1b08e1cb6822cb64
KieranBrett <brett.kieran@hotmail.com>
2020-07-17 19:32:56 +1200

    Updated transitions, padding on contentPane and also sizing of sidebar

--- assets/sideBar.js
+++ assets/sideBar.js
@@ -3,24 +3,26 @@ let contentPane = document.getElementById('contentPane');
 let navButton = document.getElementById('navButton');
 
 function minimizeBar() {
-    sideBar.style.left = '-270px'; // Gives smooth transition effect to move entire div off screen than set hidden
+    sideBar.style.left = '-18%'; // Gives smooth transition effect to move entire div off screen than set hidden
     contentPane.style.marginLeft = '0px';
     contentPane.style.paddingLeft = '10%';
+    contentPane.style.paddingRight = '10%'
     sideBar.style.visibility = 'hidden';
 }
 
 function expandBar() { 
     sideBar.style.left = '0px' // Setting div to be on screen
-    contentPane.style.marginLeft = '260px'; // Setting margin for content pane
-    contentPane.style.paddingLeft = '0%';
+    contentPane.style.marginLeft = '15%'; // Setting margin for content pane
+    contentPane.style.paddingLeft = '2.5%';
+    contentPane.style.paddingRight = '2.5%';
     sideBar.style.visibility = 'visible'; // Showing sidebar
 }
 
 navButton.onclick = () => {
-    if (sideBar.style.left == '-270px'){ // If the div is off the screen
-        expandBar();                   // Expand it
+    if (sideBar.style.left == '0px'){ // If the div is on the screen
+        minimizeBar();                   // Minimize It
     }
     else{
-        minimizeBar();                  // Minimize It
+        expandBar();                  // Expand it
     }
 }
